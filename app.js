06a0a6f43bec1766c39cab4234dc904bad09c30a
andrea.ybanez <andrea.ybanez@i2btech.com>
2019-09-16 21:35:55 -0500

    actualizando primera portada

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-var tween = TweenMax.to("#box", 3, {
-   
-    x: '-=350', // Tween to the current x value minus 50
-     y: '+=100', // Tween to the current y value plus 50// registra el valor en cada uno actualizacion }  
-     ease:Back.easeOut
-}); 
-    // onComplete: myFunction,
-    TweenMax.to(".h3", 2, { x: '+=30wh',display:'inline-block'}); 
-    TweenMax.to(".h4", 2, { x: '+=30wh',display:'inline-block'});
-    TweenMax.to(".h3", 3,{rotation:'390'})
-    TweenMax.to(".h4", 3,{rotation:'-380'})
-    TweenMax.to(".parrafo", 3,{ ease: Bounce.easeOut, y: 100 })
+$('.carrusel-section').slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true
+  });
